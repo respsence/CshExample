9d18434bb53b345815d9fedaab4b746e2e0d9e36
RespSence <negmetov@gmail.com>
2017-08-26 17:10:47 +0300

    Enums

---
+++ 008_Enums/002_Covariant/ArrayCovariant2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Противоречие между системой типов CLI и системой типов C#. 
+// (ковариантность массивов типов-значений несогласована)
+
+namespace Covariant
+{
+    class Program
+    {
+        static void Main()
+        {
+            uint[] array = new uint[3];
+
+            Console.WriteLine("array  {0} {1}", array is uint[], array is int[]);
+
+            object @object = array;
+
+            Console.WriteLine("object {0} {1}", @object is uint[], @object is int[]);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
