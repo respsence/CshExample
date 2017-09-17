3246d6cd9e67453d6f8570ea857f145923369de9
RespSence <negmetov@gmail.com>
2017-09-17 11:23:10 +0300

    String, Format, RegularExpressions

---
+++ 022_Text/001 - String/000_CreateString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CreateString
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // "Hello"
+            System.String s = "Hello";
+            
+            // "-----------------------------------------------"
+            String s2 = new string('-',20);
+
+            //"Hello-----------------------------------------------"
+            s += s2;  //s = s + s2;
+
+            //"5"
+            string s4 = 5.ToString();
+
+            //"1 + 2 = 3"
+            string s5 = String.Format("{0} + {1} = {2}", 1, 2, 1 + 2);
+
+        }
+    }
+}
